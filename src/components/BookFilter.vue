97b88cf0ef759d70dae6dfb277a379af5fae7d97
klogpok <kloleg@gmail.com>
2017-06-21 12:07:55 +0300

    added book-filter comp

---
+++ src/components/BookFilter.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="book-filter">
+    <h2>Books Filter</h2>
+    <input type="text" v-model="bookFilter.byText" placeholder="Filter By Name" >
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'book-filter',
+    data() {
+      return {
+        bookFilter: { byText: '', byMaxPrice: 1000 }
+      }
+    },
+    created() {
+      this.$watch('bookFilter.byText', function (newVal, oldVal) {
+        this.$emit('set-filter', this.bookFilter);
+      })
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
