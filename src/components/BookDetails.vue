e84958ce3decf0d57420e188af322a96f7da93f1
klogpok <kloleg@gmail.com>
2017-06-21 12:04:22 +0300

    added some comps

---
+++ src/components/BookDetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="popup book-details">
+    <button @click="closeMe">x</button>
+    <h1>Details of: {{book.title}}</h1>
+    <img src="http://via.placeholder.com/250x150">
+    <h3>\${{book.price}}</h3>
+    <p>{{book.description}}</p>
+    <button @click="requestNextBook">Next > </button>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'book-details',
+    props: ['book'],
+    data () {
+    },
+    methods: {
+      closeMe() {
+        console.log('close me!');
+        this.$emit('close');
+      },
+      requestNextBook() {
+        console.log('Requesting Next');
+        this.$emit('next');
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
